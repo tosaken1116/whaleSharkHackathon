0424b09b2f8933c12229329c763299e5de2a6d54
tosaken1116 <nagato20021122@gmail.com>
2023-04-21 15:03:13 +0900

    add:createThemeを追加 #18

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,24 @@
 import Layout from "@/components/Layout";
 import { createApolloClient } from "@/libs/createClient";
+import { createMuiTheme } from "@/libs/theme";
 import "@/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import "@babel/polyfill";
+import { ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
     const client = createApolloClient();
+    const theme = createMuiTheme();
     return (
         <RecoilRoot>
             <ApolloProvider client={client}>
-                <Layout>
-                    <Component {...pageProps} />
-                </Layout>
+                <ThemeProvider theme={theme}>
+                    <Layout>
+                        <Component {...pageProps} />
+                    </Layout>
+                </ThemeProvider>
             </ApolloProvider>
         </RecoilRoot>
     );
