23f633b0ace172bb2b6e0abe0a3c2b5757ae2707
tosaken1116 <nagato20021122@gmail.com>
2023-04-23 12:04:13 +0900

    fix:buildエラー解消 #27

--- src/components/Meeting/MeetingLog.tsx
+++ src/components/Meeting/MeetingLog.tsx
@@ -4,8 +4,7 @@ import MarkDownPreview from "./MarkDownPreview";
 export default function MeetingLog({ log, title }: Partial<MeetingLog>) {
     return (
         <>
-            {/* <MarkDownPreview rawText={"## hoge#huga"} /> */}
-            <MarkDownPreview rawText={log ?? ""} title={title} />
+            <MarkDownPreview rawText={log ?? ""} title={title ?? ""} />
         </>
     );
 }
